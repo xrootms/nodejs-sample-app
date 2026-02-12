7323b501117a345db9df7a5acd5376f0b4570834
Saifuddin Md <mdsaifuddin.ek@gmail.com>
2026-02-12 21:59:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,6 @@ app.get("/success", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, console.log("Listening on port" + process.env.HOST));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
